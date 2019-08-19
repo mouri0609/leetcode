ce8b6a143b320b77ffd732e3072fe6706187644c
mouri0609 <1055045739@qq.com>
2019-08-19 22:45:16 +0800

    1

--- src/test/xiaoHongshu/Main2.java
+++ src/test/xiaoHongshu/Main2.java
@@ -11,24 +11,22 @@ public class Main2 {
             votes[i] = in.nextInt();
         }
 
-        int count = 1;
-        int[] dp = new int[n];
-        dp[0] = votes[0];
-        dp[1] = Math.max(votes[1],dp[0]);
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = votes[0];
 
-        if (dp[0]==0&&dp[1]==0){
-            count = 0;
-        }
-
-        for (int i=2;i<n;i++){
-            dp[i] = Math.max(dp[i-1],dp[i-2]+votes[i]);
-            if (dp[i] !=dp[i-1]){
-                count++;
+        int[] result = new int[n+1];
+        result[0] = 0;
+        result[1] = 1;
+        for (int i=2;i<=n;i++){
+            dp[i] = Math.max(dp[i-1],dp[i-2]+votes[i-1]);
+            if (dp[i]==dp[i-1]){
+                result[i] = result[i-1];
+            }else {
+                result[i] = result[i-2]+1;
             }
         }
-        int total = dp[n-1];
-        System.out.println(total+" "+count);
+        int total = dp[n];
+        System.out.println(total+" "+result[n]);
     }
 }
-// 5    0 0 0 1 0
-//        1 2 3 1 3
--- src/test/xiaoHongshu/Main4.java
+++ src/test/xiaoHongshu/Main4.java
@@ -1,6 +1,13 @@
 package test.xiaoHongshu;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.Scanner;
+
+/*
+T是回合数
+H是怪物的血量
+ */
 
 public class Main4 {
     public static void main(String[] args) {
@@ -8,15 +15,74 @@ public class Main4 {
         int N = in.nextInt();
         int T = in.nextInt();
         int M = in.nextInt();
-        int[] blood = new int[N];
+        int[] H = new int[N];
         for (int i=0;i<N;i++){
-            blood[i] = in.nextInt();
-
+            H[i] = in.nextInt();
         }
 
-        if (N>=T){
-            System.out.println(-1);
+        System.out.println(solution(H, T, M));
+    }
+
+    public static int solution(int[] H, int T, int M) {
+        if (T < H.length) return -1;
+        Arrays.sort(H);
+        int high = H[H.length - 1];
+        if (!canBeat(H, T, M, high)) return -1;
+
+        int low = 0;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (canBeat(H, T, M, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
         }
 
+        return low;
+    }
+
+    public static boolean canBeat(int[] H, int T, int M, int X) {
+        if (X == 0) {
+            int sum = 0;
+            for (int h : H) {
+                sum += h;
+            }
+            if (sum <= T) return true;
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+
+        for (int i = H.length - 1; i >= 0; i--) {
+            if (H[i] >= X) {
+                int time = H[i] / X;
+                if (M >= time) {
+                    T -= time;
+                    M -= time;
+                    int rest = H[i] - time * X;
+                    if (rest != 0) pq.offer(rest);
+                } else {
+                    int rest = H[i] - M * X;
+                    T -= M;
+                    M = 0;
+                    if (rest != 0) pq.offer(rest);
+                }
+            } else {
+                pq.offer(H[i]);
+            }
+        }
+
+        while (M != 0 && !pq.isEmpty()) {
+            pq.poll();
+            M--;
+            T--;
+        }
+
+        while (T > 0 && !pq.isEmpty()) {
+            int time = pq.poll();
+            T -= time;
+        }
+
+        return pq.isEmpty();
     }
 }
